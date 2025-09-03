b48608a2ab9342394d11e0144335c23be1f4f54c
Guto Marcicano <caugmar@gmail.com>
2025-09-03 11:58:16 -0300

    Inclui Balances do Graham Nelson para o mesmo fim.
    Por ser bem menor pode ser um melhor começo.

---
+++ curses-release-16-recreation/CursesR16.sol
@@ -0,0 +1,303 @@
+The Solution to Curses 1.6
+
+Table of Contents:
+
+1)  The straight forward walkthru,
+2)  Explanations and clue location,
+3)  History of the Meldrews - Contents,
+4)  Dictionary of Mythology - Contents,
+5)  Lagachable Objects,
+6)  Amusing things to do,
+7)  Different ways to open the medicine bottle.
+
+1) The straight forward walkthru
+
+N to the old winery; worry about the key later.   W, S.  Look under the
+sheets.  Push the radio N to Aunt Jemima's lair and turn it on.  W to the 
+potting room.  Wait (`Z') until the radio starts playing music.  Take the 
+gloves.  Wear the gloves.  E, E.  Open the demijohn.    S, S, SE, E.  
+Examine the insulation rolls.  Take the new battery.  Open the torch.  
+Empty it.  Put the new battery in the torch.  Close it.  S.  Take the 
+rucksack.
+
+N.  Open the cupboard.  N.  Take the painting.  Take the gas mask.  S, W, 
+NW, N, N.  Take the nasty red battery and the map from the demijohn.  E.  
+Take the wrench.  Enter the dumbwaiter.  Take the wishbone.  Pull the rope.  
+Get out of the dumbwaiter.  Take the mouse. S.  Drop the mouse.  Type: 
+`mouse, W',  `hole, W', `hole, W', `hole, W', `hole, N', `hole, W', `hole, 
+N',  `hole, S', `hole, E', `hole, S', `hole, E', `hole, E', `hole, E', and 
+`hole, E'.  Take the attic key.  N.  Enter the dumbwaiter.  Pull the rope.
+Get out.  Wear the gas mask.  N.  Unlock door with the brass key.  Open 
+door.  S.  Enter the dumbwaiter.  Pull the rope.  Get out.  W, S.  Examine 
+the teachest.  Look inside the teachest.  E.  Lie down.  Sleep.  E, N.  
+Take the mascot.  The word `lagach'  will be `heard'.  It's a magic word 
+that allows you to travel between different artifacts.  Drop the mascot.  
+S, W, SW and E.  Get up. Take the dictionary and scarf.  W, S.  Take all 
+(from the cupboard).  Open the parcel. N, N, W.  Examine the calendar.  
+Flip through the calendar (to February) until you find Merlyn's Hat 
+daisies.  Remember the colour.  W.  Give the box of chocolates to Jemima.  
+Remove gas mask.  Answer her question `say yellow'.  E, E, S, S, SE, W.  
+Pull the cord.  Examine the postcard.  Take the flash.  Open flash.  Put 
+the nasty red battery in the flash.  Close flash.  Look at the photograph.  
+Read about Roger in the history book.
+
+E, E and N.  Turn the crank.  U, NW.  Take the key.  Give the wishbone to 
+the ghost.  SE, D.   Put the torch and the brass key into the fireplace.  
+Drop all.  Enter the fireplace.  D.  Take all.  Unlock the hatch with the 
+brass key.  Open the hatch.  D,  E.  Turn off wheel.  Turn wheel.  Enter 
+the dumbwaiter and pull the rope.  Turn the wheel.  Enter the dumbwaiter.  
+Pull the rope.  Pull the rope.  Get out of the dumbwaiter.  W, S, S, SE, E, 
+N.  Drop all.  Take the rucksack.  Take all.  S, W, NW, N, N, E.  Turn 
+wheel off.  Turn wheel.  Drop the medicine bottle in the shaft.  Turn the 
+wheel.  Enter the dumbwaiter.  Take the tablet.  Get out of the dumbwaiter.  
+W, W, W, E, E, S, S, SE, D.  Clean the glass ball.  W.  Take the wrench.  
+Fix the pipe.  Take the books.  Read the romance novel.  Read the book of 
+poetry.
+
+N, U.  Push the bell.  Say even.  N, U.  Take the Tarot cards.  D, E.  
+Examine the mural.  W, S, W.  Take the silk handkerchief.  Wave it.  Board 
+the boat.  Say time.  NW.  Climb the tree.  Look at the maze.  D, W, W. 
+Take all.  Board the roller.  Turn it on.  E, E, E, W, N, N, W, W, N, N, N, 
+N, W, W, W.  Get off the roller.  Take the miniature.  Board the roller.  
+E, E, E, S, S, S, S, E, E, S, S.  Turn off the roller.  Get off the roller.  
+Wear gas mask.  S, D.  Turn wheel.  Enter dumbwaiter.  Pull the rope.  Pull 
+the rope.  Get out of the dumbwaiter.  W, S, S, SE, S, S.  Turn on the 
+slide projector.  Turn the dial.  Put the Ace of Cups into the slot.  S.  
+Search the crates.  Take the mounted bottle.  Pull the anchor on the ship.  
+Put the stick ship into the mounted bottle.  Examine the ship.  Climb the 
+mast.  Take the flag.  Go port.  Take all.  Go fore.  Take the green 
+branch.  Go aft.  Go aft.  Turn the wheel.  D.
+
+Take the Ace of Cups.  Put the Fool into the slot.  N, N, E, S, E, NE, N.  
+Examine the cross.  Read about Alison in the history book. S, SW, W.  Push 
+the south wall.  S.  Lie down.  Put the flag on the bed.  Sleep.  Wait.  
+Once the man is gone, go E and E.  Turn the wheel.  Pinch yourself (pinch 
+me).  Get out of bed.  Take the stick from the rucksack.  Look in the 
+mirror.  Where's the stick?  Wave the stick.  Wave the timber spar.   Break 
+the window.  S.  Look under the window.  Take the gold key.  N, N, N, W, 
+NW, N.  Close the door.  Take the box.  Open the door.  Unlock the box with 
+the gold key.  Open the box.  Take the four leaf clover.  Wave the clover.
+N, E.  Enter the dumbwaiter.  Pull the rope.  Pull the rope.  Get out of 
+the dumbwaiter.  N, E, E.  Take the charcoal sketch.  Break the frame.  
+Take the letter and the sketch.  Open the coffin.  Wear the gloves (if you 
+took them off).  Read the inscription.  Read the letter.  Put a mahogany 
+rod into the coffin.  Close the coffin.  Take the `named' rod.  Do this for 
+every mahogany rod you have now and in the future.  This will be referred 
+to as `convert the rod'.  Take the scroll.  SE.  Hang the painting on the 
+hook here.  Search the umbrella stand.  Take the sceptre.
+
+U, U.  Read about 1420 in the map of Hamburg.  Turn the door.  NE.  Push 
+the ball SW.  Push the ball S.  Take the tarot box.  Open it.  E.  Remove 
+gas mask.  Break the cabinet.  Take all from the cabinet.  Take the tablet 
+and the Rod of Returning (if they are in the rucksack).  Strike the Rod of 
+Returning.  Wait  until druids take you.  Eat the tablet.  Take the 
+Shepherd's crook.  Point the Rod of Returning at yourself (point returning 
+at me).  Return to the souvenir's room.  Take the fool.  Put the miniature 
+in the slot.  Strike the Rod of Returning.  S.  E, E, E, E, E, S, E, S, E, 
+E, S, S.  (In whitewashed area).  W.  Squeeze the weed killer bottle.  E, 
+N, N, W, W, N, W, N, W, W, W, W, W.  Take the bean pole.  Point the Rod of 
+Returning at yourself.
+
+Return back to the library storage room and take the poetry book.  E, N, 
+NW, N, N, E.  Turn the wheel.  Enter the dumbwaiter.  Pull the rope.  Pull 
+the rope.  Get out of the dumbwaiter.  N, NW.  Board  the roller.  Turn on 
+the roller.  N, N, E, E, E.  Get off the roller.  Take the marble rose.  D.  
+Examine the mural.  Read the poetry book.  Take the tarot cards from the 
+Tarot box.  N, U.  Put the 8 of wands on the deck.  Put the maiden on the 
+deck.  Put the star on the deck.  Push the bell.  N, U.  Take the tarot 
+cards.  D, S ,W.  Board the boat and say time.  Return to the octagon room.  
+Convert the quarterstaff, the crook, the bean pole and the 8 of wands.  SE.  
+Type: lagach, painting.  Then lagach the artifacts until you return to the 
+bronze mural.  Take the poetry book.  Strike the Rod of Bronze.  Point it 
+at the mural.  D.  Take the key.  E, E.  Read the tombstone. W, W, S.  
+Smell.  Smell.  D.  Turn off the switch.  U, W.  Examine the panel.  
+Examine the bronze wall.  Strike the Rod of Bronze.  Point it at the wall.  
+N.  Examine the panel.  Slide i.  Slide k.  Slide c.  Slide a.  Slide n.  
+Slide e.  Slide l.  Slide o.  Slide s.  Slide t.  S.  Slide k.  Slide k.  
+E, U, U, U.  Board the roller.  W, W, W, S, S, E.  Turn off the roller.  
+Get off the roller.
+
+Take the golden orb.  W, SE, S, E, E, SE, U, U, W, S, SE, S, S.  Put the 
+Maiden into the slot. S, U, E, S.  Strike the Rod of Fire.  Point it at the 
+wall.  Strike the Rod of Luck.  Point it at yourself.  S, S.  Take the 
+copper coin.  SE.  Take the inscribed stone.  Examine the inscribed stone.  
+SW.  Wake Homer.  Say Agamemnon.  Say Ptolemy.  Say yellow.  D.  SW.  Push 
+the statue NE.  NW.   Push Dionysus SE and SW.  NE, NE.   Push Demeter  SW 
+and NW.  SE.  Push Poseidon NE.  SW.  Put the inscribed stone the opening.  
+D.  Take the amber gem.  U, U, NE, NW, N, N, N.  Strike the Rod of 
+Husbandry.  Point it at the goats.  E.  Take the fig.  S.  Give the coin to 
+the bartender.  Take the dessert.  N, W, W, D.  Give Andromeda the dessert.  
+U, SW.  Play the pan-pipes.  Put the fig in the urn.  Type: priestess, tell 
+me about fig.  Write down her response because they contain coordinates you 
+will need later.
+
+Return to the souvenir's room (via the Rod of Returning, etc.).  Put the 
+castle card in the slot.  S.  Look under the table.  Examine the scarf.  
+Pull the blue wire.  Pull the green wire.  Pull the black wire.  Pull the 
+red wire.  Wait until the timer runs out.  Strike the Rod of Returning and 
+point it at yourself.  Return to the souvenir's room.  Put the star in the 
+slot.  S, D.  Examine the frieze.  U.  Open the flash.  Set the timer.  Put 
+the timer in the flash.  Close the flash.  Put the flash in the device.  
+Wait until the timer goes off.  E, E.  Lagach the mosaic and keep 
+`lagaching' until you get back to the frieze.  U.  Take all.
+Return to the souvenir's room.  Put the charcoal sketch into the slot.  
+Find Austin in the house.  Keep pushing him towards the souvenir's room.  
+Jump.  S, N.  Drop the smooth round stone into the grating.  S, SE, SW, S.  
+Examine the writings.  N.  Blow the bird whistle.  Take the cloak.  Wear 
+the cloak.  NE, NE, SE, S.  Type: anoint me.  N, SE.  Take the spindle.  
+NW.  Wait until the messenger arrives.  Take the tubes.  Swap the scrolls 
+in the tubes.  E.  Give the Kappa tube to Callimachus.  S.  Give the Alpha 
+tube to Apollonius.  Take all.  N, W, NW, N, W.  Take the stone.  Take the 
+key. (If you want) Play dice with the sailor to learn the ancient words for 
+one to six.  They are, one:thu, two:zal, three:si, four:ca, five:mach, and 
+six:huth.  E, S, SW, NW, N.  Unlock the grating with the rusty key.  Open 
+the grating.  D.  Take the heart.  Board the skiff.  Sail until you reach 
+the Garden Stream.
+
+S, E, E.  Convert the spindle and the hairband.  U.  SE, U, U, W, S, SE, E, 
+S, E, NE, D.  Wave the green branch.  Take the nuts.  NW.  Examine the 
+mosaic.  Take the wooden ball.  Lagach to  Mad Isaac's painting.  NE, E, N.  
+Take the croquet mallet.  S, E.  Use the coordianates the priestess gave 
+you and walk <X> paces west and <Y> paces south (other possibilities 
+include east and south, east and north, and west and north).  Dig with the 
+gardening implement.    Unlock the strongbox with the gothic key.  Open it.  
+Take the astrolabe.  Fill the hole.  Drop the wooden ball.  Hit it with the 
+croquet mallet.  NW.  Put the nuts in the crack.  S.  Blow the bird 
+whistle.  Take the gold watch.
+
+S, W, SW.  Lagach back to Alexandria.  N.  Put the amber gem into the 
+socket.  D. Lie down on the couch.  Sleep. Lie down on the couch.  Sleep. 
+Lie down on the couch.  Sleep.  Lie down on the couch.  Sleep.  Wait and 
+watch the French soldiers.  Wait until they leave.  Get up and go W 
+repeatedly until you have returned to the `present'.  Twist the sphinx's 
+nose.  W.  Take the model.  W.  Move in various directions until you 
+dislodge a greenish oak.  Take the oak.  Put the sceptre into the first 
+socket.  Turn it to `si'.  Put the sceptre into the second socket.  Turn it 
+to `huth'.  Put the sceptre into the third socket.  Turn it to `thu'.  
+Enter the coffin.  Close the lid.  Close the lid.  U.
+
+W, W, U.  Lagach back to Alexandria.  N, NE, SE.  Remove the cloak.  Turn 
+the cloak.  Wear the cloak.  Open the door.  Enter the tower.  Put the 
+green oak timber onto the table.  NW.  Remove the cloak.  Turn the cloak.  
+N, N, N.  Remove the cloak.  N.  Wear the purple sash.  N, NE, E,  S.  
+Type: anoppe.  Put the astrolabe on the mounting.  Look in the eyepiece.  
+Take the hand.  D, D, W.  Put the hand on the statue.  Tighten the hand.  
+Put the skull on the statue.  Tighten the skull.  Put the heart in the 
+statue.  Strike the Rod of Fire.  Point it at the statue.  Point E.  E.  
+Point E.  E.  Give the marble rose to the knight.  Show the keepsake to the 
+knight.  Point W.  W.  Type: knight, open moonstone.  Type: open moonstone.  
+Type: knight, open moonstone.  Point D.  Strike the Rod of Ice.  Point it 
+at the Rod of Love.  Take the Rod of Love.  Take the Rod of Love.  S.  
+Hypnotise Evans with the watch.  Type: evans, give me the mascot.  S, SW, 
+W, S.  Take the model of the ugly animal from the rucksack.  Look in the 
+mirror.  N, E, NE, N.
+
+Take the golden orb from the rucksack.  Clean it.  Strike the Rod of 
+Sacrifice.  Point it on black.  S, SW, W, N, W,NW, N, N, E.  Turn the 
+wheel.  Enter the dumbwaiter.  Pull the ropes. Get out of the dumbwaiter.  
+W.  Unlock the door with the wrought iron key.  Open the door.  NW.  Put 
+all the rods, except the Rod of Eternity, into the lemniscus.  Put the orb 
+in the opening.  Strike the Rod of Infinity.  Point it at the Lemniscus.  
+Take the torch.  W, D.  Swing the rope.  Take the daisy.  Wear the daisy.  
+U, E, NE, E, S.  Take the horn.  S, E.  Take the sandals.  Wear the 
+sandals.  W, W.  Open the cover.  Put the torch in the well.  D, E.  Blow 
+the horn.  Wave the horn.  Strike the Rod of Language.  Point it at 
+yourself.  Listen until the druids are finished talking.  W, U.  Wait until 
+the saxon spy in thrown into the tent.  Take the pole.  E.  Take the blue 
+stone.  Wave the tent pole.  Strike the Rod of Returning.  Point it at 
+yourself.
+
+Read the book of poetry.  E, D.  Wave the blue stone.  Give the fifty-franc 
+note to the man.  Say carte.  U, W, W.  Board the boat.  Say time.  SE, E.  
+Enter the dumbwaiter.  Pull the rope.  Pull the rope.  Get out of the 
+dumbwaiter.  W, S, D.  Congratulations!  You've scored 544 out of 550 
+points!.
+
+2) Explanations and clue locations
+
+It is important to read the history of the Meldrews ask check any new name 
+you come across.  For instance, when you look at the photograph in the 
+Darkroom,  then read about Roger in the history book.
+
+Reading about Isaac in the history book gives the clue about Merlyn 
+(Merlin).
+
+Looking at the maze from the top of the Family Tree shows on spot which is 
+inaccessible.  This gives the clue are where to squeeze the killer weed 
+bottle in the past.
+
+From the Stone Cross, if you look up the name Gerard in the history book 
+and have read that Marie Swelldon is the author of the romantic novel, then 
+you should figure out that Marie Swelldon is the nom de plume of Roger 
+Meldrew's wife.  Marie Swelldon is an anagram for Alison Meldrew.  Reading 
+about Alison provides the clue to push the south wall in the Dead End.
+
+The inscribed stone and the dog-eared letter provide the game player with 
+the necessary translations to complete the game.
+
+Either looking in Alison's mirror or checking your inventory (Merlyn's hat) 
+give clues to the possession of a rod.
+
+The mural in the crypt gives the clue of stacking the deck in the 
+consulting room.
+
+Looking up Henri in the history book give the clue to solve the panel 
+puzzle in Contraption Room.
+
+Clues to answer Homer's questions can be found both in Alexandria and in 
+the dictionary.
+
+The priestess, if you make food offerings, can give additional clues to 
+solving Doktor Stein and defusing the bomb.
+
+The papyrus fragment and the smooth stone provide the solution to the maze 
+in the palace.
+
+The clue to using the Rod of Sacrifice in the orb comes from a) noticing 
+you are actually in a chess board and b) reading about Helene, then Anton 
+in the history book.
+
+3) History of the Meldrews - Contents
+
+Alison, Anton, Capability, Ebeneezer, Gerard, Helene, Henri, Isaac, Joshua, 
+Peter and Roger.
+
+4) Dictionary of Mythology - Contents
+
+Aeschyus, Agememnon, Alexander, Alexandria, Andromeda, Aphrodite, Apollo, 
+Apollonius, Ares, Arthur, Athene, Callimachus, Chlamys, Demeter, Diana, 
+Dionysus, Eraina, Eumenides, Galita, Hades, Helicon, Hephaetus, Hera, 
+Homer, Iliad, Kraken, Leda, Loxias, Memelaus, Odyssey, Odysseus, Pan, 
+Perseus, Pharos, Pluto, Poseidon, Ptomemy, Pythian, Selenae, Sosostris, 
+Syrinx, Ulysses, Zeus.
+
+5) Lagachable Objects
+
+Mosaic, both murals, still life, painting (when placed on the hook), frieze 
+and the writings.
+
+6) Amusing things to do
+Drop the robot mouse where Austin can get it.
+Shut Austin out of the attic with the trap door.
+Ram or knock on various doors.
+Cast all the rods on yourself.
+Ask the demon (or angel - I didn't tell you where to use the Rod of 
+Stalking!) about heaven and hell.
+Eat a genuine Ekmek special.
+
+7) Different ways of opening the medicine bottle
+
+By dropping it down the empty dumbwaiter shaft.
+Putting it at the foot of the shaft and dropping the dumbwaiter on 
+it.
+Running over it with the garden roller.
+Dropping it from the foot of the beanstalk.  (Rats, I gave away a 
+clue!)
+Dropping it from the top of the mast.
+Aunt Jemima can open it.
+The jaws of Dobbin (Doktor Stein's goat).
+
+This walkthrough was written by Marc Leduc (100435,772) and he can be e-
+mailed at 100435.772@compuserve.com
+
+
